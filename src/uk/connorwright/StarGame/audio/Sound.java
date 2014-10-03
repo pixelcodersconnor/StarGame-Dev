9afe765994b3abf72c5e59ef46cec6819313647f
Connor Wright <connor@epicgamingmc.co.uk>
2014-10-03 18:36:15 +0100

    Added more sound effects
    Added enemy hit and game win sound effects

--- src/uk/connorwright/StarGame/audio/Sound.java
+++ src/uk/connorwright/StarGame/audio/Sound.java
@@ -7,10 +7,13 @@ public class Sound {
 	public static final Sound laserFired = new Sound("/music/laser.wav");
 	public static final Sound selectEffect = new Sound("/music/select.wav");
 	public static final Sound loseSound = new Sound("/music/lose.wav");
-	//public static final Sound enemyHit = new Sound("/enemy_hit.wav");
-	//public static final Sound playerHit = new Sound("/player_hit.wav");
-	public static final Sound shaunEgg1 = new Sound("/music/easter_eggs/shaun_1.wav");
-	public static final Sound shaunEgg2 = new Sound("/music/easter_eggs/shaun_2.wav");
+	public static final Sound enemyHit = new Sound("/music/sfx/enemy_hit.wav");
+	public static final Sound gameWin = new Sound("/music/sfx/game_win.wav");
+	// public static final Sound playerHit = new Sound("/player_hit.wav");
+	public static final Sound shaunEgg1 = new Sound(
+			"/music/easter_eggs/shaun_1.wav");
+	public static final Sound shaunEgg2 = new Sound(
+			"/music/easter_eggs/shaun_2.wav");
 	private AudioClip clip;
 
 	private Sound(String name) {
